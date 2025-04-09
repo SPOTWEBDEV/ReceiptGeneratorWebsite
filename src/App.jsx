3296bf7e4500a709a228a36bc65b8e9ab582c1ec
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-04-09 14:21:25 +0100

    add login , register , policy , term , forgot password file and their design

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,34 @@ import Contact from "./pages/Contact";
 import Example from "./pages/Example";
 import Error404 from "./pages/ErrorPage/404";
 import Editor from "./pages/Editor";
+import Register from "./pages/clients/Register";
+import Login from "./pages/clients/Login";
+import TermsOfService from "./pages/TermsOfService";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import ForgetPassword from "./pages/clients/ForgetPassword";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* landing page routs */}
         <Route path="/" element={<Home />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/example" element={<Example />} />
+        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/forgot-password" element={<ForgetPassword />} />
+
+        {/* Not-Found routs */}
         <Route path="*" element={<Error404 />} />
+
         <Route path="/editor" element={<Editor />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        
       </Routes>
     </BrowserRouter>
   );
