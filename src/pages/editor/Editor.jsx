472cefdfe52dc18d76772bd6ae7639b1d2f1d201
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-04-17 22:51:17 +0100

    work on the editor

--- src/pages/editor/Editor.jsx
+++ src/pages/editor/Editor.jsx
@@ -1,51 +1,45 @@
 import React, { useState } from 'react';
-import editor from  './editor.module.css';
+import editor from './editor.module.css';
 
 import { AccordionItem_invoiceType } from '../../components/AccordionItem'
 
 const Editor = () => {
     const [sideBar, setSideBar] = useState(true)
-    
+
     return (
         <div>
-            <nav className={editor.nav}>
-                <h1 className={editor.nav_h1}>SmartInvestX</h1>
-                <div className={editor.button_groups}>
-                    <button className={editor.button}>save</button>
-                    <button className={editor.button}>preview</button>
+
+            <div className={editor.navbar}>
+                <div className={editor.breadcrumbs}>
+                    <a href="#">Home</a>
+                    <span>›</span>
+                    <a href="#">Invoices</a>
                 </div>
-            </nav>
 
-            <div className={editor.content}>
-                {sideBar && <div className={editor.sidebar}>
-                    <div>
-                        <AccordionItem_invoiceType removeSide={setSideBar} className="AccordionItem" title="Payment Invoice" />
-
+                <div className={editor.searchBar}>
+                    <div className={editor.searchIcon}>
+                        <input
+                            type="text"
+                            placeholder="Search thousands of templates"
+                        />
                     </div>
-                </div>}
+                </div>
 
-                <div className={editor.main}>
-                    <div className={editor.editor}>
-                        <form className={editor.form}>
-                            <div className="card">
-                                <div className="color">
-                                     
-                                </div>
-                                <div className="p-2">
-                                    <label htmlFor="">Message</label>
-                                    <textarea className='w-100 border-none p-2'></textarea>
-                                </div>
-                            </div>
-                            
-                        </form>
-                    </div>
-
-                    <div className={editor.editor}>
-                        <p>Preview Here</p>
-                        
+                <div className={editor.icons}>
+                    <div className={editor.icon}>⚙️</div>
+                    <div className={editor.icon}>🔔</div>
+                    <div className={editor.profile}>
+                        <div className={editor.profileCircle}>S</div>
+                        <div className={editor.profileInfo}>
+                            <div><strong>Personal</strong></div>
+                            <small>spotwebdev.com</small>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
+
         </div>
     );
 };
