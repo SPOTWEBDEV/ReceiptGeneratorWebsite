a4b397751909fc5543cc9b808f84facad76efd82
somto ogbodo <somtoogbodo2007@gmail.com>
2025-06-16 14:45:08 +0100

    commit

--- landing/index.php
+++ landing/index.php
@@ -1,17 +1,24 @@
+<?php
+
+include("./server/connection.php")
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
-  
-<!-- Mirrored from admin.pixelstrap.net/zono/template/landing-page.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 12 Jun 2025 12:30:26 GMT -->
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Zono admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
-    <meta name="keywords" content="admin template, Zono admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="description" content="<?php echo $sitename    ?> admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, <?php echo $sitename    ?>  admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
     <link rel="icon" href="./assets/images/favicon.png" type="image/x-icon">
     <link rel="shortcut icon" href="./assets/images/favicon.png" type="image/x-icon">
-    <title>Zono - Premium Admin Template</title>
+    <title><?php echo $sitename ?> - Premium Admin Template</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Outfit:400,400i,500,500i,700,700i&amp;display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&amp;display=swap" rel="stylesheet">
@@ -46,7 +53,9 @@
         <div class="custom-container">
           <div class="row">
             <div class="col-12 p-0">
-              <nav class="navbar navbar-light p-0" id="navbar-example2"><a class="navbar-brand" href="javascript:void(0)"><img class="img-fluid" src="./assets/images/logo/landing-logo.png" alt=""></a>
+              <nav class="navbar navbar-light p-0" id="navbar-example2"><a class="navbar-brand" href="javascript:void(0)">
+                <img style="width: 100px;" class="img-fluid" src="./assets/images/logo.png" alt=""></a>
+                <!-- <h1 class="img-fluid" style="font-size: 20px;">Smar InoiceX</h1> -->
                 <ul class="landing-menu nav nav-pills">
                   <li class="nav-item menu-back">back<i class="fa fa-angle-right"></i></li>
                   <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
@@ -79,13 +88,11 @@
               <div class="home-text">
                 <div class="main-title"> 
                   <div class="d-flex align-items-center gap-2">
-                    <div class="d-flex gap-2 align-items-center"><img class="img-fluid" src="./assets/images/landing/landing-home/vector/rocket.png" alt="rocket">
-                      <p class="m-0 project-name">Kick Start Your Project Using</p>
-                    </div>
+                
                   </div>
                 </div>
-                <h2>Zono Developer Friendly Admin Dashboard For Your Business<img class="line-text img-fluid" src="./assets/images/landing/landing-home/vector/line.png" alt="line"></h2>
-                <p class="description-name">Zono is perfect admin template for any business. it has all features and modules to create your amazing C- panel. this template for selling default, ecommerce.</p>
+                <h2><?php echo $sitename ?> Developer Friendly Admin Dashboard For Your Business<img class="line-text img-fluid" src="./assets/images/landing/landing-home/vector/line.png" alt="line"></h2>
+                <p class="description-name"><?php echo $sitename ?> is perfect admin template for any business. it has all features and modules to create your amazing C- panel. this template for selling default, ecommerce.</p>
                 <div class="docutment-button"> <a class="btn bg-secondary" href="index-2.html" target="_blank">View Demo</a><a class="btn bg-light txt-dark" href="https://docs.pixelstrap.net/admin/zono/document/" target="_blank">Documentation</a></div>
               </div>
             </div>
