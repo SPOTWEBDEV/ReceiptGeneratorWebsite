c30b726801645d1c6aac999539005499d95395f5
bestkind444 <ayoguchimezie00@gmail.com>
2025-06-20 16:23:58 +0000

    done deal, no be small thing

--- user/include/footer.php
+++ user/include/footer.php
@@ -2,10 +2,10 @@
           <div class="container-fluid">
             <div class="row">
               <div class="col-md-6 p-0 footer-copyright">
-                <p class="mb-0">Copyright 2024 © <?php  echo $sitename ?> theme by pixelstrap.</p>
+                <p class="mb-0">Copyright 2024 © <?php  echo $sitename ?>Smart InvoiceX – All rights reserved</p>
               </div>
               <div class="col-md-6 p-0">
-                <p class="heart mb-0">Hand crafted &amp; made with
+                <p class="heart mb-0">
                   <svg class="footer-icon">
                     <use href="../assets/svg/icon-sprite.svg#heart"></use>
                   </svg>
